69417225a403745029575ddce01af024c97e1b3e
Guilherme Ferreira Lenzolari <guilhermeferreiralenzolari@MacBook-Pro-de-Guilherme-2.local>
2025-06-25 12:41:56 -0300

    changes color of dropdown text to blue when nothing is selected

--- Projeto/src/components/DropdownButton/DropdownButton.jsx
+++ Projeto/src/components/DropdownButton/DropdownButton.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import "./DropdownButton.css"
+import dados from "../../../../dados.json"
 
 const DropdownButton = ({ children, open, toggle }) => {
+
+  function isJsonKey(text) {
+    const upperText = String(text).toUpperCase();
+    const upperKeys = Object.keys(dados).map(k => k.toUpperCase())
+
+    return upperKeys.includes(upperText)
+  }
+
+  const isKey = isJsonKey(children)
+
   return (
     <div 
         onClick={toggle} 
-        className={`dropdown-btn ${open ? "button-open" : null}`}>
+        className={`dropdown-btn ${open ? "button-open" : null} ${isKey ? "textGray": ""}`}>
       {children}
       <span className='toggle-icon'>
         {open ? <FaChevronUp/> : <FaChevronDown />}
